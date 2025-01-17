8571fe075b3a23f0fdd66ec72d79160935593a80
Ricardo Nevarez Jr <ricardo@nevarez.cloud>
2025-01-17 15:48:34 -0500

    Updated useEffect

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 
     const url = "https://function.nevarez.cloud/api/views"
 
-    const [count, setCount] = useState([null]);
+    const [count, setCount] = useState(null);
 
     const english_ordinal_rules = new Intl.PluralRules("en", {type: "ordinal"});
     const suffixes = {
@@ -24,8 +24,8 @@ function App() {
     
     useEffect(() => {
       view_count()
-    },[])
-    
+    })
+
     const view_count = async () => {
         const res = await fetch(url, {
             method: "POST",
