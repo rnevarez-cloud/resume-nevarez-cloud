3f7cc62d3b4c9b24dec2f2003085ff9fb46af4ee
Ricardo Nevarez Jr <ricardo@nevarez.cloud>
2025-01-20 17:14:22 -0500

    Reverted changes to implement react-router

--- src/index.js
+++ src/index.js
@@ -4,9 +4,4 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.render(<App />, document.getElementById('root'));
-root.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-);
+ReactDOM.render(<App />, document.getElementById('root'));
