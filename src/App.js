876c2f6a7976863fe7e9837d15784a441de26e3a
Ricardo Nevarez Jr <ricardo@nevarez.cloud>
2025-01-20 10:58:33 -0500

    Removing phone number

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ function App() {
             {count && <h4>You are the {count} visitor!</h4>}
             <h1 class="center">Ricardo Nevarez Jr</h1>
             <div>
-                <p><a href="ricardo@nevarez.cloud">ricardo@nevarez.cloud</a> | (937) 527-7022 | <a href="https://www.linkedin.com/in/rnevarezjr/">https://www.linkedin.com/in/rnevarezjr/</a> | <a href="https://github.com/rnevarez-cloud">https://github.com/rnevarez-cloud</a></p>
+                <p><a href="ricardo@nevarez.cloud">ricardo@nevarez.cloud</a> | <a href="https://www.linkedin.com/in/rnevarezjr/">https://www.linkedin.com/in/rnevarezjr/</a> | <a href="https://github.com/rnevarez-cloud">https://github.com/rnevarez-cloud</a></p>
             </div>
         </div>
         <div>
