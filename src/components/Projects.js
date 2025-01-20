47e36e97ee766ac83d0fa4627020c3dc04c52175
Ricardo Nevarez Jr <ricardo@nevarez.cloud>
2025-01-20 17:07:15 -0500

    Added additional details to skills section

---
+++ src/components/Projects.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../index.css';
+
+function Projects() {
+
+  return (
+    <>
+    <body>
+        <div class="center">
+            <h1 class="center">Projects</h1>
+        </div>
+    </body>
+  </>
+  ) 
+}
+
+export default Projects;
