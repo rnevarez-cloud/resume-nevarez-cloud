1f4ec8f8846e09653c677abd31808295b7115473
Ricardo Nevarez Jr <ricardo@nevarez.cloud>
2025-01-21 08:35:19 -0500

    Updated createRoot import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
-import { createRoot } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
