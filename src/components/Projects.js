8f70de2ce20c7665acb7b2ec1d9c8447d1d6c317
Ricardo Nevarez Jr <ricardo@nevarez.cloud>
2025-01-22 13:53:26 -0500

    Updated path of post

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../index.css';
 import Markdown from 'react-markdown';
-import post from '../posts/projects/2022-01-01-projects.md;'
+import post from './posts/projects/2025-01-22-azure.md'
 
 function Projects() {
   return (
