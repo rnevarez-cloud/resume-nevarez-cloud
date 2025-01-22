88433a88d6c3e675c6de1732c0a320eb49e0ae96
Ricardo Nevarez Jr <ricardo@nevarez.cloud>
2025-01-22 13:48:22 -0500

    Testing markdown

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import '../index.css';
+import Markdown from 'react-markdown';
+import post from '../posts/projects/2022-01-01-projects.md;'
 
 function Projects() {
   return (
     <>
       <div class="center">
             <h1 class="center">Projects</h1>
+            <Markdown>{post}</Markdown>
       </div>
   </>
   ) 
