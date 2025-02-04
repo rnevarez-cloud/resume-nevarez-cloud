7ab70f85f72d4f0fc093870f1406b878100a7327
Ricardo Nevarez Jr <ricardo@nevarez.cloud>
2025-02-04 14:05:41 -0500

    Added remarkHtml rendering

--- src/components/Markdown.js
+++ src/components/Markdown.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Markdown from 'react-markdown';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";
+import remarkHtml from 'remark-html';
 
 export function MarkdownRenderer({ children: markdown }) {
   return (
     <Markdown
+      remarkPlugins={[remarkHtml]}
       components={{
         code({ className, children, ...rest }) {
           const match = /language-(\w+)/.exec(className || "");
