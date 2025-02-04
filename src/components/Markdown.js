a2080e09c333853ef153d4e954ab67020a14f8e5
Ricardo Nevarez Jr <ricardo@nevarez.cloud>
2025-02-04 14:09:45 -0500

    Reverting changes until I can figure out how to get expand to work

--- src/components/Markdown.js
+++ src/components/Markdown.js
@@ -2,12 +2,10 @@ import React from 'react';
 import Markdown from 'react-markdown';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";
-import remarkHtml from 'remark-html';
 
 export function MarkdownRenderer({ children: markdown }) {
   return (
     <Markdown
-      remarkPlugins={[remarkHtml]}
       components={{
         code({ className, children, ...rest }) {
           const match = /language-(\w+)/.exec(className || "");
