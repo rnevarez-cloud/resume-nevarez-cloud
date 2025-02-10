c103b6112639d052be0acbd8a506c95c2284711a
Ricardo Nevarez Jr <ricardo@nevarez.cloud>
2025-02-10 15:55:23 -0500

    Change to fetch

--- src/components/Scores.js
+++ src/components/Scores.js
@@ -3,25 +3,18 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 
 function Scores() {
-      const [scores, setScores] = useState([]);
-  
-      useEffect(() => {
-        scoresReq()
-      },[])
+  const [scores, setScores] = useState([]);
 
-      const url = "https://scores.nevarez.cloud/api/scores"
-          
-      const scoresReq = async () => {
-        const res = await fetch(url, {
-              method: "GET",
-              headers: {
-              "Content-type": "*/*; charset=UTF-8",
-              "Access-Control-Allow-Origin": "https://scores.nevarez.cloud"
-              }
-          });
-          
-          setScores(await res.json());
-        };
+  useEffect(() => {
+    scoresReq()
+  },[])
+
+  const url = "https://scores.nevarez.cloud/api/scores"
+      
+  const scoresReq = async () => {
+    const res = await fetch(url);  
+    setScores(await res.json());
+  }  
   
   return (
     <>
