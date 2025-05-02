72d5a745dcd8b4b23edbb582498fd3db1bd72a40
Ricardo Nevarez Jr <ricardo@nevarez.cloud>
2025-05-02 12:48:01 -0400

    Experimenting with the Scores.js page to read the score from a markdown file instead of CosmosDB.

--- src/components/Scores.js
+++ src/components/Scores.js
@@ -1,46 +1,11 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import '../index.css';
+import React from 'react';
+import Markdown from 'react-markdown';
 import emoji from 'react-easy-emoji';
 
-const url = "https://scores.nevarez.cloud/api/scores"
-
-function Scores() {
-  const [scores, setScores] = useState(() => {
-    const savedScores = sessionStorage.getItem("scores");
-    return savedScores ? JSON.parse(savedScores) : [];
-  });
-
-  const scoresReq = useCallback(async () => {
-    const res = await fetch(url);
-    const data = await res.json();
-    setScores(data);
-    sessionStorage.setItem("scores", JSON.stringify(data));
-
-  }, []);
-
-  useEffect(() => {
-    if(scores.length === 0) {
-      scoresReq();
-    }
-  },[scores, scoresReq]);
-
+export function MarkdownRenderer({ children: markdown }) {
   return (
-    <>
-      <div className="scores">
-      {scores.map((data) => (
-          <div key={data.game}>
-            <br />
-            <div className="center">
-              <p>{data.puzzle}</p>
-              <div id={data.game} className="score">
-                {emoji(data.score)}
-              </div>
-            </div>
-          </div>
-      ))}
-      </div>
-  </>
-  ) 
+    <Markdown>
+      {emoji(markdown)}
+    </Markdown>
+  );
 }
-
-export default Scores;
