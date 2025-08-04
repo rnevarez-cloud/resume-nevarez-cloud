a1862db329edfa5c94f5070bad6eec823cb04090
Ricardo Nevarez Jr <ricardo@nevarez.cloud>
2025-08-04 14:17:30 -0400

    Updated next.config.ts and package.json for static web app deployment

--- next.config.ts
+++ next.config.ts
@@ -4,5 +4,9 @@ const nextConfig: NextConfig = {
   output: 'export', // Outputs a Single-Page Application (SPA)
   distDir: 'build', // Changes the build output directory to `build`
 }
- 
+
+module.exports = {
+    output:"standalone",
+}
+
 export default nextConfig
