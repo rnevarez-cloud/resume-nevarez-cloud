a3fed19bd228f9ea0dd960edea54e93a28e14cb9
Ricardo Nevarez Jr <ricardo@nevarez.cloud>
2025-08-05 11:05:05 -0400

    Fixed missing import

--- src/[[...slug]]/client.js
+++ src/[[...slug]]/client.js
@@ -2,7 +2,7 @@
  
 import dynamic from 'next/dynamic'
  
-const App = dynamic(() => import('../app/App'), { ssr: false })
+const App = dynamic(() => import('../app/App.js'), { ssr: false })
  
 export function ClientOnly() {
   return <App />
